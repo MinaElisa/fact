7e2c656ddcab8ad0365e5dca939eba16120671e1
admin <dvn4may@outlook.com>
2023-12-15 19:58:13 +0800

    20231215

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,7 +6,8 @@ window.onload=function(){
     var e_emergency = document.getElementsByClassName('emergency')[0];
     var icons1 = e_emergency.querySelectorAll("[id='icon']");
     for(var i=0;i<icons1.length;i++){
-        icons1[i].style.backgroundImage='url("./icon/icon'+(i+1)+'.png")';
+        icons1[i].style.backgroundImage='url("./icon/icon_index'+(i+1)+'.png")';
     }
-    console.log("[index.js]Icon images was procced, "+icons1.length+" images been set.");
+    console.log("[index.js]Image batches finished, "+icons1.length+" icons been set.");
 }
+console.log("[index.js]index.js ended.")
