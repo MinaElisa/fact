2293d5631c748b4d421f1cd37b0a547407011cbc
admin <dvn4may@outlook.com>
2023-12-17 14:18:37 +0800

    20231217

--- javascripts/index.js
+++ javascripts/index.js
@@ -8,6 +8,34 @@ window.onload=function(){
     for(var i=0;i<icons1.length;i++){
         icons1[i].style.backgroundImage='url("./res/icon/icon_index'+(i+1)+'.png")';
     }
-    console.log("[index.js]Image batches finished, "+icons1.length+" icons been set.");
+
+    var quickcontent = document.getElementsByClassName("quickcontent")[0];
+    var textfolds = quickcontent.getElementsByTagName('li');
+    for(var i=0;i<textfolds.length;i++){
+        if(textfolds[i].clientHeight < textfolds[i].scrollHeight){
+            textfolds[i].insertAdjacentHTML('afterend','<button id="btnfold">Expand</button>');
+        }
+    }
+
+    var btn = quickcontent.getElementsByTagName('button');
+    for(var x=0;x<btn.length;x++){
+        btn[x].addEventListener('click', function() {
+            if(window.getComputedStyle(this.previousElementSibling,null).height==='100px'){
+                this.previousElementSibling.style.height="auto";
+                this.innerHTML='Clip';
+            }else{
+                this.previousElementSibling.style.height="100px";
+                this.innerHTML='Expand';
+            }
+        });
+    }
+    // var btn = quickcontent.getElementsByTagName('button')[1];
+    // btn.addEventListener('click', function() {
+    //     if(window.getComputedStyle(textfolds[1],null).height==='100px'){
+    //         textfolds[1].style.height="auto";
+    //     }else{
+    //         textfolds[1].style.height='100px';
+    //     }
+    // });
+
 }
-console.log("[index.js]index.js ended.")
