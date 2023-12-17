2f7ee06ba9a1f73343149b1739c2b12824650102
admin <dvn4may@outlook.com>
2023-12-17 16:13:17 +0800

    20231217

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,33 +9,25 @@ window.onload=function(){
         icons1[i].style.backgroundImage='url("./res/icon/icon_index'+(i+1)+'.png")';
     }
 
-    var quickcontent = document.getElementsByClassName("quickcontent")[0];
-    var textfolds = quickcontent.getElementsByTagName('li');
-    for(var i=0;i<textfolds.length;i++){
-        if(textfolds[i].clientHeight < textfolds[i].scrollHeight){
-            textfolds[i].insertAdjacentHTML('afterend','<button id="btnfold">Expand</button>');
-        }
-    }
-
-    var btn = quickcontent.getElementsByTagName('button');
-    for(var x=0;x<btn.length;x++){
-        btn[x].addEventListener('click', function() {
-            if(window.getComputedStyle(this.previousElementSibling,null).height==='100px'){
-                this.previousElementSibling.style.height="auto";
-                this.innerHTML='Clip';
-            }else{
-                this.previousElementSibling.style.height="100px";
-                this.innerHTML='Expand';
-            }
-        });
-    }
-    // var btn = quickcontent.getElementsByTagName('button')[1];
-    // btn.addEventListener('click', function() {
-    //     if(window.getComputedStyle(textfolds[1],null).height==='100px'){
-    //         textfolds[1].style.height="auto";
-    //     }else{
-    //         textfolds[1].style.height='100px';
+    // 已废弃的'文本折叠展开'功能代码
+    // var quickcontent = document.getElementsByClassName("quickcontent")[0];
+    // var textfolds = quickcontent.getElementsByTagName('li');
+    // for(var i=0;i<textfolds.length;i++){
+    //     if(textfolds[i].clientHeight < textfolds[i].scrollHeight){
+    //         textfolds[i].insertAdjacentHTML('afterend','<button id="btnfold">Expand</button>');
     //     }
-    // });
+    // }
 
+    // var btn = quickcontent.getElementsByTagName('button');
+    // for(var x=0;x<btn.length;x++){
+    //     btn[x].addEventListener('click', function() {
+    //         if(window.getComputedStyle(this.previousElementSibling,null).height==='100px'){
+    //             this.previousElementSibling.style.height="auto";
+    //             this.innerHTML='Clip';
+    //         }else{
+    //             this.previousElementSibling.style.height="100px";
+    //             this.innerHTML='Expand';
+    //         }
+    //     });
+    // }
 }
