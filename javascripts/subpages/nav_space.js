c7cd60bcbe8ccd31dc0af86655bc95a5801a8e87
admin <dvn4may@outlook.com>
2023-12-17 18:03:41 +0800

    20231217

--- javascripts/subpages/nav_space.js
+++ javascripts/subpages/nav_space.js
@@ -6,7 +6,7 @@ window.onload=function(){
     var e_emergency = document.getElementsByClassName('banner')[0];
     var imgs = e_emergency.querySelectorAll("[id='image']");
     for(var i=0;i<imgs.length;i++){
-        imgs[i].style.backgroundImage='url("../res/images/pic'+(i+1)+'.jpg")';
+        imgs[i].style.backgroundImage='url("../res/images/subpages/banner ('+(i+1)+').png")';
     }
     console.log("[index.js]Image batches finished, "+imgs.length+" icons been set.");
 }
