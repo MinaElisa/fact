6d9bc0d5e3afd048a62c434700c0e7ebb4711670
admin <dvn4may@outlook.com>
2023-12-19 19:46:35 +0800

    min

--- javascripts/pages/template1.js
+++ javascripts/pages/template1.js
@@ -1,15 +1,13 @@
 console.log("[template1.js]index.js started.")
 
 window.onload=function(){
-
     // 根据当前html名称去追加navlogo的文本
     var temp1 = location.href.split("/");
     var temp2 = temp1.slice(temp1.length-1, temp1.length).toString(String).split(".");
     var logo = document.getElementsByClassName('LOGO')[0];
     var text= document.createElement('p');
     text.style.display='inline';
-    text.style.fontSize='.8em';
+    text.style.fontSize='40px';
     text.innerText=' | '+String(temp2.slice(0,1));
     logo.appendChild(text);
-    // logo.appendChild(document.createTextNode("|"+String(temp2.slice(0,1))));
 }
